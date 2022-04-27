5ac0e7e75d58b69a60ee0031c4bfa307178ffba8
m-arc <m-arc@192.168.1.8>
2022-04-27 08:42:07 -0300

    Atualização

--- src/lambdas/CauculoTeste2.java
+++ src/lambdas/CauculoTeste2.java
@@ -4,8 +4,19 @@ public class CauculoTeste2 {
 
 	public static void main(String[] args) {
 
-		Calculo soma = (x, y) -> { return x + y; };
+		Calculo soma = (x, y) -> {
+			return x + y;
+		};
 		
 		System.out.println(soma.executar(2, 3));
+
+		soma = (x, y) -> x * y;
+		System.out.println(soma.executar(2, 3));
+		
+		soma = (x, y) -> x - y;
+		System.out.println(soma.executar(3, 2));
+		
+		soma = (x, y) -> x / y;
+		System.out.println(soma.executar(3, 2));
 	}
 }
