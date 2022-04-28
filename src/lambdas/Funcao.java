dd6204913c27c760d998117dba36718421be3d7a
m-arc <m-arc@192.168.1.8>
2022-04-28 18:51:58 -0300

    Atualização

--- src/lambdas/Funcao.java
+++ src/lambdas/Funcao.java
@@ -8,14 +8,13 @@ public class Funcao {
 		
 		// Interface funcional Function
 		Function<Integer, String> parOuImpar = numero -> numero % 2 == 0 ? "Par" : "Impar";
-		
-		System.out.println(parOuImpar.apply(32));
-		
+			
 		Function<String, String> oResultadoE = valor -> "O resulto e: " + valor;
 		
 		Function<String, String> empolgado = valor -> valor + "!!!!";
 		
-		String resultadoFinal = parOuImpar.andThen(oResultadoE).apply(32);
+		String resultadoFinal = parOuImpar.andThen(oResultadoE).andThen(empolgado).apply(32);
 		System.out.println(resultadoFinal);
+		System.out.println(parOuImpar.apply(32));
 	}
 }
