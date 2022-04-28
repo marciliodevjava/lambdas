39d94ff27989dc5acb221b2ba16e45cfa5a205b0
m-arc <m-arc@192.168.1.8>
2022-04-28 10:12:30 -0300

    Interface funcional Predicato

---
+++ src/lambdas/Produto.java
@@ -0,0 +1,15 @@
+package lambdas;
+
+public class Produto extends Object{
+
+	final String nome;
+	final double peco;
+	final double desconto;
+	
+	public Produto(String nome, double peco, double desconto) {
+		super();
+		this.nome = nome;
+		this.peco = peco;
+		this.desconto = desconto;
+	}	
+}
