905f7814c7695f5dde61d24ba889110055e8e365
m-arc <m-arc@192.168.1.8>
2022-04-28 10:38:16 -0300

    Atualizar

--- src/lambdas/Predicado.java
+++ src/lambdas/Predicado.java
@@ -6,7 +6,7 @@ public class Predicado {
 
 	public static void main(String[] args) {
 		
-		Predicate<Produto> isCaro = prod -> (prod.peco *( 1 - prod.desconto)) >= 750;
+		Predicate<Produto> isCaro = prod -> (prod.preco *( 1 - prod.desconto)) >= 750;
 		
 		Produto produto = new Produto("Notebook", 3893.89, 0.15);
 		// função para testar
--- src/lambdas/Produto.java
+++ src/lambdas/Produto.java
@@ -1,15 +1,20 @@
 package lambdas;
 
-public class Produto extends Object{
+public class Produto extends Object {
 
 	final String nome;
-	final double peco;
+	final double preco;
 	final double desconto;
-	
-	public Produto(String nome, double peco, double desconto) {
+
+	public Produto(String nome, double preco, double desconto) {
 		super();
 		this.nome = nome;
-		this.peco = peco;
+		this.preco = preco;
 		this.desconto = desconto;
-	}	
+	}
+
+	public String toString() {
+		double precoFinal = this.preco * (1 - this.desconto);
+		return "Nome: " + this.nome + " tem o preço R$ " + precoFinal;
+	}
 }
