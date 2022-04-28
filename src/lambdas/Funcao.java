88058c6a5998f89b09cf32e9ff7ee1d796563c70
m-arc <m-arc@192.168.1.8>
2022-04-28 18:54:03 -0300

    Atulização

--- src/lambdas/Funcao.java
+++ src/lambdas/Funcao.java
@@ -12,9 +12,13 @@ public class Funcao {
 		Function<String, String> oResultadoE = valor -> "O resulto e: " + valor;
 		
 		Function<String, String> empolgado = valor -> valor + "!!!!";
+		Function<String, String> duvida = valor -> valor + "????";
 		
 		String resultadoFinal = parOuImpar.andThen(oResultadoE).andThen(empolgado).apply(32);
 		System.out.println(resultadoFinal);
+		
+		String resultadoFina2 = parOuImpar.andThen(oResultadoE).andThen(duvida).apply(33);
+		System.out.println(resultadoFina2);
 		System.out.println(parOuImpar.apply(32));
 	}
 }
