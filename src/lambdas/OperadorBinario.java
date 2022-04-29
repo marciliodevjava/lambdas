a9eac5330bf0334990d66e96bd14e6f89b24f2a1
m-arc <m-arc@192.168.1.8>
2022-04-28 21:35:39 -0300

    Funcition BinariOperator

--- src/lambdas/OperadorBinario.java
+++ src/lambdas/OperadorBinario.java
@@ -1,13 +1,28 @@
 package lambdas;
 
+import java.util.Scanner;
+import java.util.function.BiFunction;
 import java.util.function.BinaryOperator;
 
 public class OperadorBinario {
 
 	public static void main(String[] args) {
-
+		// Operador binario
 		BinaryOperator<Double> media = (n1, n2) -> (n1 + n2) / 2;
 
 		System.out.println(media.apply(9.8, 5.7));
+
+		BiFunction<Double, Double, String> resultado = (n1, n2) -> (n1 + n2) / 2 >= 7.0 ? "Aprovado" : "Reprovado";
+
+		Scanner entrada = new Scanner(System.in);
+		System.out.println("Veriique se você está aprovado");
+		System.out.println("Digite a primeira nota: ");
+		String nota1 = entrada.next().replace(",", ".");
+		double notaA = Double.parseDouble(nota1);
+		System.out.println("Digite a segunda nota: ");
+		String nota2 = entrada.next().replace(",", ".");
+		double notaB = Double.parseDouble(nota1);
+
+		System.out.println(resultado.apply(notaA, notaB));
 	}
 }
